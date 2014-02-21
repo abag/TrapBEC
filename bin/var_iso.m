411e218d526f3a63255a1ae889db9f3ad36166cd
Andrew Baggaley <a.w.baggaley@gmail.com>
2014-02-21 10:22:52 +0000

    minor changes

--- bin/var_iso.m
+++ bin/var_iso.m
@@ -3,7 +3,7 @@ global Nx Ny Nz Psi Lx Ly Lz
 whitebg('w')
 temp_Psi=smooth3(Psi);
 temp_Psi=permute(temp_Psi,[3 2 1]);
-p = patch(isosurface(1-abs(temp_Psi.^2),.7));
+p = patch(isosurface(1-abs(temp_Psi.^2),.6));
 isonormals(1-abs(Psi),p)
 set(p,'FaceColor','g','EdgeColor','none');
 camlight 
@@ -12,6 +12,7 @@ daspect([1,1,1])
 view(3); 
 alpha(0.3)
 axis([ 1 Nx 1 Ny 1 Nz])
-axis off
+axis on
+box on
 set(gca,'FontSize',16)
-zoom(1.5)
+camproj('perspective')
